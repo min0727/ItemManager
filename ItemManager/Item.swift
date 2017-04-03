5310b0bb294cc8aff3c5dfa08fc92a095654d958
min0727@home <mino727@c-able.ne.jp>
2017-04-03 18:13:59 +0900

    initial

---
+++ ItemManager/Item.swift
@@ -0,0 +1,9 @@
+//
+//  Item.swift
+//  ItemManager
+//
+//  Created by 松崎勝治 on 2017/03/29.
+//  Copyright © 2017年 ___松崎　勝治___. All rights reserved.
+//
+
+import Foundation
---
+++ ItemManager/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ItemManager
+//
+//  Created by 松崎 勝治 on 2017/03/27.
+//  Copyright (c) 2017 ___松崎　勝治___. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+    }
+
+
+
+}
